086f52029cfb3c45fbbeaad948b803998ddb6657
Ryan Williams <ryan.williams@tiledb.com>
2025-02-13 23:05:49 +0000

    wip dsk.py csr memray

--- apis/python/dsk.py
+++ apis/python/dsk.py
@@ -36,17 +36,23 @@
 # peaks = { n: json.load(open(f"nose-{n}k.json", 'r'))['metadata']['peak_memory'] for n, nz in nnz.items() }
 # { n: peaks[n] / nz for n, nz in nnz.items() }
 # ```
-from os import makedirs
-from os.path import join
+import json
+import re
+from os import makedirs, remove
+from os.path import join, exists, splitext
 
 import dask
 from click import option, group, argument
 import pyarrow as pa
+from humanize import naturalsize
 from pyarrow import feather
 from scipy.sparse import csr_matrix
 from somacore import AxisQuery
 from tiledbsoma import Experiment, SparseNDArray, SOMATileDBContext
 from tiledbsoma._query import load_daskarray
+
+import memray
+from utz import err, run
 
 
 @group
@@ -54,14 +60,29 @@ def cli():
     pass
 
 
+CHUNK_SIZE_RGX = re.compile(r'(?P<k>\d+)k')
+
+
+def parse_chunk_size(ctx, param, value):
+    if m := CHUNK_SIZE_RGX.fullmatch(value):
+        return int(m['k']) * 1000
+    else:
+        return int(value)
+
+
 CENSUS = "s3://cellxgene-census-public-us-west-2/cell-census/2024-07-01/soma/census_data/homo_sapiens"
-chunk_size_opt = option('-c', '--chunk-size', type=int)
+chunk_size_opt = option('-c', '--chunk-size', callback=parse_chunk_size)
+overwrite_bin_opt = option('-f', '--overwrite-bin', is_flag=True)
+no_overwrite_memray_stats_opt = option('-F', '--no-overwrite-memray-stats', is_flag=True)
+memray_bin_opt = option('-m', '--memray-bin-path')
+no_native_traces_opt = option('-N', '--no-native-traces', is_flag=True)
 tissue_opt = option('-t', '--tissue', default='nose')
 
 # ```bash
 # dsk.py joinids -t nose nose  # Write joinids to `nose/{obs,var}.feather`
 # dsk.py csr -c 10000 nose     # Load `nose/{obs,var}.feather`, fetch first 10k obs rows (and all vars), build a CSR chunk, print shape+nnz
 # ```
+
 
 @cli.command
 @tissue_opt
@@ -85,34 +106,85 @@ def joinids(tissue, joinids_dir):
 
 @cli.command
 @chunk_size_opt
+@overwrite_bin_opt
+@no_overwrite_memray_stats_opt
+@memray_bin_opt
+@no_native_traces_opt
 @argument('joinids_dir')
 def csr(
     chunk_size,
+    overwrite_bin,
+    no_overwrite_memray_stats,
+    memray_bin_path,
+    no_native_traces,
     joinids_dir,
 ):
-    obs_joinids_path = join(joinids_dir, "obs.feather")
-    var_joinids_path = join(joinids_dir, "var.feather")
-    obs_tbl = feather.read_table(obs_joinids_path)
-    all_obs_joinids = obs_tbl['obs_joinids']
-    obs_joinids = all_obs_joinids[:chunk_size].to_numpy().tolist()
-    var_tbl = feather.read_table(var_joinids_path)
-    var_joinids = var_tbl['var_joinids'].to_numpy().tolist()
-    shape = (len(obs_joinids), len(var_joinids))
-    tiledb_config = {
-        "vfs.s3.no_sign_request": "true",
-        "vfs.s3.region": "us-west-2",
-    }
-    soma_ctx = SOMATileDBContext(tiledb_config=tiledb_config)
-    uri = f"{CENSUS}/ms/RNA/X/raw"
-    with SparseNDArray.open(uri, context=soma_ctx) as arr:
-        tbl = arr.read((obs_joinids, var_joinids)).tables().concat()
-    soma_dim_0, soma_dim_1, data = [col.to_numpy() for col in tbl.columns]
-    obs_joinid_idx_map = {obs_joinid: idx for idx, obs_joinid in enumerate(obs_joinids)}
-    obs = [obs_joinid_idx_map[int(obs_joinid)] for obs_joinid in soma_dim_0]
-    var_joinid_idx_map = {var_joinid: idx for idx, var_joinid in enumerate(var_joinids)}
-    var = [var_joinid_idx_map[int(var_joinid)] for var_joinid in soma_dim_1]
-    csr = csr_matrix((data, (obs, var)), shape=shape)
-    print(f"{csr.shape}, {csr.nnz}")
+    if memray_bin_path is None:
+        if chunk_size % 1000 == 0:
+            name = f'{chunk_size // 1000}k'
+        else:
+            name = f'{chunk_size}'
+        memray_bin_path = join(joinids_dir, f'{name}.bin')
+        err(f"memray logging to {memray_bin_path}")
+
+    regen_bin = True
+    native_traces = not no_native_traces
+    if exists(memray_bin_path):
+        if overwrite_bin:
+            err(f"Removing existing {memray_bin_path}")
+            remove(memray_bin_path)
+        else:
+            regen_bin = False
+            err(f"{memray_bin_path} exists, skipping `memray run` (use `-f` to overwrite)")
+
+    if regen_bin:
+        with memray.Tracker(memray_bin_path, native_traces=native_traces):
+            obs_joinids_path = join(joinids_dir, "obs.feather")
+            var_joinids_path = join(joinids_dir, "var.feather")
+            obs_tbl = feather.read_table(obs_joinids_path)
+            all_obs_joinids = obs_tbl['obs_joinids']
+            obs_joinids = all_obs_joinids[:chunk_size].to_numpy().tolist()
+            var_tbl = feather.read_table(var_joinids_path)
+            var_joinids = var_tbl['var_joinids'].to_numpy().tolist()
+            shape = (len(obs_joinids), len(var_joinids))
+            tiledb_config = {
+                "vfs.s3.no_sign_request": "true",
+                "vfs.s3.region": "us-west-2",
+            }
+            soma_ctx = SOMATileDBContext(tiledb_config=tiledb_config)
+            uri = f"{CENSUS}/ms/RNA/X/raw"
+            with SparseNDArray.open(uri, context=soma_ctx) as arr:
+                tbl = arr.read((obs_joinids, var_joinids)).tables().concat()
+            soma_dim_0, soma_dim_1, data = [col.to_numpy() for col in tbl.columns]
+            obs_joinid_idx_map = {obs_joinid: idx for idx, obs_joinid in enumerate(obs_joinids)}
+            obs = [obs_joinid_idx_map[int(obs_joinid)] for obs_joinid in soma_dim_0]
+            var_joinid_idx_map = {var_joinid: idx for idx, var_joinid in enumerate(var_joinids)}
+            var = [var_joinid_idx_map[int(var_joinid)] for var_joinid in soma_dim_1]
+            csr = csr_matrix((data, (obs, var)), shape=shape)
+            err(f"CSR: {csr.shape}, {csr.nnz}")
+
+    name, _ = splitext(memray_bin_path)
+    memray_json_path = f"{name}.stats.json"
+    run(
+        'memray', 'stats', '--json',
+        *([] if no_overwrite_memray_stats else ['-f']),
+        '-o', memray_json_path,
+        memray_bin_path)
+    with open(memray_json_path, 'r') as f:
+        stats = json.load(f)
+    peak = stats['metadata']['peak_memory']
+    err(f"Peak memory use: {peak} ({naturalsize(peak)})")
+    out_json_path = f'{name}.json'
+    with open(out_json_path, 'w') as f:
+        json.dump(
+            dict(
+                shape=csr.shape,
+                nnz=csr.nnz,
+                peak_mem=peak,
+            ),
+            f,
+            indent=2,
+        )
 
 
 @cli.command
